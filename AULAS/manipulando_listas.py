2c6190b4742c3be721965853eefc2fbcf24974db
Carlos Felipe <161951854+Carlos-Felipe-Victorio@users.noreply.github.com>
2025-08-18 22:59:18 -0300

    Create manipulando_listas.py

---
+++ AULAS/manipulando_listas.py
@@ -0,0 +1,16 @@
+frutas = ["maça", "uva", "laranja", "abacaxi", "mamão"]
+print(frutas)
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+print(frutas[4])
+
+frutas.append("tangerina")
+print(frutas)
+
+frutas.remove("uva")
+print(frutas)
+
+novas_frutas = frutas.pop(0)
+print(novas_frutas)
